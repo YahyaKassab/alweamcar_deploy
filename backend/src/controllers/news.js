4128e0955c573132a382947fccf12d1a74e34289
Yahya Kassab <ya7yakassab@gmail.com>
2025-03-27 14:39:45 +0200

    add details to getAllNews

--- backend/src/controllers/news.js
+++ backend/src/controllers/news.js
@@ -26,11 +26,7 @@ exports.getAllNews = asyncHandler(async (req, res, next) => {
   const skip = (page - 1) * parsedLimit;
 
   const total = await News.countDocuments();
-  const news = await News.find()
-    .select('-details -__v')
-    .skip(skip)
-    .limit(parsedLimit)
-    .sort('-date');
+  const news = await News.find().select('-__v').skip(skip).limit(parsedLimit).sort('-date');
 
   res.status(200).json({
     success: true,
